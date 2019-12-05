72d247a93de9fbf2f73cb4e55f155ffb90f74b14
abouneau <axel.bouneau@etu.u-bordeaux.fr>
2019-12-05 21:15:22 +0100

    ok

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
-var express = require('express');
+const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 
-var indexRouter = require('./routes/index');
-var bodyParser = require('body-parser')
+const indexRouter = require('./routes/index')
 
-var app = express();
-
-
-// Set up mongoose connection
-var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
-// view engine setup
+const app = express()
 
 app.set('view engine', 'ejs')
 
-app.use('public', express.static('/public'))
-app.use('views', express.static('/views'))
+app.use('/public', express.static('public'))
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
-app.listen(3000, function(){
-    console.log('App listening on port 3000 !')
+const port = 3000
+
+app.listen(port, () => {
+  console.log('Server is listenning on port ' + port)
 })
-
-module.exports = app;
