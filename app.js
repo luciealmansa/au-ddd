2884e37459d92e942271ef332484efa6b56aea70
abouneau <axel.bouneau@etu.u-bordeaux.fr>
2019-12-05 20:50:08 +0100

    ok

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-var createError = require('http-errors');
 var express = require('express');
 
 var indexRouter = require('./routes/index');
-var path = require('path');
+var bodyParser = require('body-parser')
 
 var app = express();
 
@@ -18,31 +17,16 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
+app.set('view engine', 'ejs')
+
+app.use('public', express.static('/public'))
+app.use('views', express.static('/views'))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(3000, function(){
     console.log('App listening on port 3000 !')
